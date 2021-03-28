60fd9938a435dca36083e568cca354c6d6d69492
ayshmnmm <ayushmanmuduli19@gmail.com>
2021-03-28 08:01:59 +0530

    Added functionality of adding and moving tasks

---
+++ beta/main.js
@@ -0,0 +1,50 @@
+function newtask(task) {
+    if (task.value) {
+    
+        var t = document.createElement("li");
+        t.innerHTML = String(task.value).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').concat('<span class="next">></span>');
+        task.value = "";
+        console.log(t);
+        document.getElementById("tdl").appendChild(t);
+        refreshnex();
+
+    }
+    
+}
+
+function refreshnex() {
+    var nexbut = document.getElementsByClassName("next");
+    var i;
+    for (i = 0; i < nexbut.length; i++) {
+    nexbut[i].onclick = function() {
+    var cn = this.parentElement.parentElement.id;
+    var elem = this.parentElement;
+    if (cn === "tdl") {
+        console.log('yes');
+        document.getElementById("idl").appendChild(elem);
+    }
+    else if (cn === "idl") {
+        document.getElementById("ddl").appendChild(elem);
+    }
+    else {
+        elem.style.display = "none" ;
+      }
+     }
+    }
+    var task = document.getElementById("t-input");
+    task.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+        newtask(task);
+    }
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    refreshnex();
+});
+
+
+
+
+
